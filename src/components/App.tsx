ca3dbfb889c444ee44a11f07e6f5be3c4755a623
Bannermanj <josephagbannerman@gmail.com>
2020-09-09 16:39:15 -0400

    add initial project files

---
+++ src/components/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./index.scss";
+
+class App extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+        <h1>Testing</h1>
+      </React.Fragment>
+    );
+  }
+}
+
+export default App;
